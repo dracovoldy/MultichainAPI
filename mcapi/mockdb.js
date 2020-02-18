e1f9533da4bdd153443ddb95fde1715f50f4b714
Mallik <abhishek.mallik@capgemini.com>
2020-02-18 21:25:18 +0530

    change mockdb

--- mcapi/mockdb.js
+++ mcapi/mockdb.js
@@ -4,6 +4,7 @@ const db = {
         { streamName: "PO_7000070001", ebeln: "7000070001", steamref: "" },
         { streamName: "PO_7000070002", ebeln: "7000070002", steamref: "" },
         { streamName: "PO_7000070010", ebeln: "7000070010", steamref: "" },
+        { streamName: "PO_9900070010", ebeln: "9900070010", steamref: "" }
     ]
 }
 
